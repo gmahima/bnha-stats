cfc5a5fdf7f61d6bc56dcc334b73efa30e1a8626
gmahima <mahimagangavarapu@gmail.com>
2020-07-25 21:47:51 +0530

    add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import "tailwindcss/dist/base.min.css"
+import {createGlobalStyle} from 'styled-components'
 
-const App = ({Component, pageProps}) => <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+   body {
+     margin: 0;
+     padding: 0;
+     font-family: Open-Sans, Helvetica, Sans-Serif;
+     text-align: center;
+  
+   }
+ `;
+ 
+const App = ({Component, pageProps}) => (
+    <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+)
 export default App
